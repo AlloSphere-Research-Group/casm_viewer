83494799a33630c4a9c604fdd861f2a791a2980e
Andres Cabrera <andres@office>
2018-08-30 16:30:02 -0700

    Add scripts and needed files for build

---
+++ app_config.cmake
@@ -0,0 +1,34 @@
+# allolib location
+set(allolib_directory allolib)
+
+# list your app files here
+set(app_files_list
+	vdv_group/al_DataScript.hpp
+  vdv_group/common.hpp
+  vdv_group/instanced_mesh.hpp
+  vdv_group/simulator.cpp
+  vdv_group/al_VASPReader.hpp
+  vdv_group/json.hpp
+  vdv_group/slice.hpp
+)
+
+# other directories to include
+set(app_include_dirs
+  ${CMAKE_CURRENT_LIST_DIR}
+)
+
+# other libraries to link
+set(app_link_libs
+)
+
+# definitions
+set(app_definitions
+)
+
+# compile flags
+set(app_compile_flags
+)
+
+# linker flags, with `-` in the beginning
+set(app_linker_flags
+)
---
+++ run.sh
@@ -0,0 +1,6 @@
+#allolib/run.sh vdv_group/simulator.cpp
+
+mkdir -p build
+cd build
+cmake .. 
+cmake --build .

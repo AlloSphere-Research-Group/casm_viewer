4d486ef8169e66c6c093b3b583830423646fc4f5
Andres Cabrera <andres@atari>
2019-04-10 12:28:34 -0700

    Updates for building error and misc functionality

--- app_config.cmake
+++ app_config.cmake
@@ -5,6 +5,9 @@ set(allolib_directory allolib)
 set(app_files_list
   vdv_group/al_DataScript.hpp
   vdv_group/common.hpp
+  vdv_group/processors.hpp
+  vdv_group/parameterspace.hpp
+  vdv_group/datasetmanager.hpp
   vdv_group/instanced_mesh.hpp
   vdv_group/simulator.cpp
   vdv_group/al_VASPReader.hpp
@@ -15,6 +18,7 @@ set(app_files_list
 # other directories to include
 set(app_include_dirs
   ${CMAKE_CURRENT_LIST_DIR}
+  ${allolib_directory}/external/Gamma
 )
 
 # other libraries to link
@@ -32,3 +36,25 @@ set(app_compile_flags
 # linker flags, with `-` in the beginning
 set(app_linker_flags
 )
+
+# ---- OpenVR
+set(OPENVR_ROOT "C:/Users/Mengyu/source/repos/openvr")
+set(OPENVR_INCLUDE_DIR "${OPENVR_ROOT}/headers")
+set(OPENVR_LIB "${OPENVR_ROOT}/lib/win64/openvr_api.lib")
+
+if(EXISTS "${OPENVR_LIB}")
+  add_definitions(-DBUILD_VR)
+  message("Building with OpenVR support")
+  list(APPEND app_include_dirs
+  ${OPENVR_INCLUDE_DIR}
+  )
+
+  # other libraries to link
+  list(APPEND app_link_libs
+  ${OPENVR_LIB}
+  )
+
+  list(APPEND app_compile_flags
+      /bigobj /F2000000
+  )
+endif()
--- run.sh
+++ run.sh
@@ -2,12 +2,26 @@
 
 #allolib/run.sh vdv_group/simulator.cpp
 
+CMAKE_BINARY=cmake
+
+if [ -d "C:\Program Files (x86)\Microsoft Visual Studio\2019" ]; then
+  GENERATOR="Visual Studio 15 2017 Win64"
+  CMAKE_BINARY="C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe"
+  echo Building for Visual Studio 16 2019 Win64
+elif [ -d "C:\Program Files (x86)\Microsoft Visual Studio\2017" ]; then
+  GENERATOR="-G 'Visual Studio 15 2017 Win64'"
+  CMAKE_BINARY="C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe"
+  echo Building for Visual Studio 15 2017 Win64
+fi
+
 mkdir -p build
 cd build
 mkdir -p Release
 cd Release
-cmake -DCMAKE_BUILD_TYPE="Release" ../.. 
-cmake --build . --config Release --target casm_viewer_run
+
+set -x
+"${CMAKE_BINARY}" -G "${GENERATOR}" -DCMAKE_GENERATOR_INSTANCE="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community" -DCMAKE_BUILD_TYPE="Release" ../..
+"${CMAKE_BINARY}" --build . --config Release --target casm_viewer_run
 
 # if [ $? == 0 ]; then
 #     cd ../bin

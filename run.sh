1aa1a434b44aea1bf2b8d4ebd8e74f9c1559d0cd
Andres Cabrera <andres@atari>
2019-04-10 17:24:22 -0700

    Further fixes to root run script

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ cd Release
 
 
 if [ -d "C:\Program Files (x86)\Microsoft Visual Studio\2017" ]; then
-    GENERATOR="-G 'Visual Studio 15 2017 Win64'"
+    GENERATOR='Visual Studio 15 2017 Win64'
     CMAKE_BINARY="C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe"
     if [ ! -f "${CMAKE_BINARY}" ]; then
       CMAKE_BINARY="C:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe"
@@ -25,16 +25,15 @@ if [ -d "C:\Program Files (x86)\Microsoft Visual Studio\2017" ]; then
 
     echo Building for Visual Studio 15 2017 Win64
 
+    set -x
     "${CMAKE_BINARY}" -G "${GENERATOR}" -DCMAKE_GENERATOR_INSTANCE="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community" -DCMAKE_BUILD_TYPE="Release" ../..
-
-
 else
+    set -x
     "${CMAKE_BINARY}" -G "${GENERATOR}" -DCMAKE_BUILD_TYPE="Release" ../..
 
 
 fi
 
-set -x
 "${CMAKE_BINARY}" --build . --config Release --target casm_viewer_run
 
 # if [ $? == 0 ]; then

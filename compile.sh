d676d9790c5b533a06c094bb06b83f3b87f69c15
Kon Hyong Kim <konhyong@gmail.com>
2021-04-22 15:36:17 -0700

    made some fixes to cpack variables

---
+++ compile.sh
@@ -0,0 +1,10 @@
+mkdir -p build/release
+cd build/release
+cmake -DCMAKE_BUILD_TYPE=Release -Wno-deprecated ../..
+cd ../..
+cmake --build build/release -j7
+cp res/macos/Info.plist bin/casm_viewer.app/Contents/
+mkdir bin/casm_viewer.app/Contents/Resources
+cp res/macos/myrioi_icon.icns bin/casm_viewer.app/Contents/Resources/
+cp res/macos/run bin/casm_viewer.app/Contents/MacOS/
+cp build/release/external/tinc/external/allolib/external/rtmidi/RtMidiConfig.cmake build/release
